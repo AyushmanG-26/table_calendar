5f1a15dcadc0e847222c3d52be607b1bc66e0e42
Ayushman Gupta <ayushman.g26@gmail.com>
2022-03-17 18:21:38 +0530

    updated to null safe for internal

--- lib/src/widgets/custom_icon_button.dart
+++ lib/src/widgets/custom_icon_button.dart
@@ -1,31 +1,33 @@
-// Copyright 2019 Aleksander Woźniak
-// SPDX-License-Identifier: Apache-2.0
+//  Copyright (c) 2019 Aleksander Woźniak
+//  Licensed under Apache License v2.0
 
-import 'package:flutter/material.dart';
+part of table_calendar;
 
-class CustomIconButton extends StatelessWidget {
-  final Widget icon;
-  final VoidCallback onTap;
-  final EdgeInsets margin;
-  final EdgeInsets padding;
+class _CustomIconButton extends StatelessWidget {
+  final Icon? icon;
+  final VoidCallback? onTap;
+  final EdgeInsets? margin;
+  final EdgeInsets? padding;
 
-  const CustomIconButton({
+  const _CustomIconButton({
     Key? key,
     required this.icon,
     required this.onTap,
-    this.margin = const EdgeInsets.all(0.0),
-    this.padding = const EdgeInsets.all(8.0),
-  }) : super(key: key);
+    this.margin,
+    this.padding,
+  })  : assert(icon != null),
+        assert(onTap != null),
+        super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: margin,
+      padding: margin!,
       child: InkWell(
         onTap: onTap,
         borderRadius: BorderRadius.circular(100.0),
         child: Padding(
-          padding: padding,
+          padding: padding!,
           child: icon,
         ),
       ),
